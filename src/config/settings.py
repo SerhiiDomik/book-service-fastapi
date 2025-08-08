ba6ad12526441529d963645bbd2ab88f02754fdf
Serhii <serhii.domik@gmail.com>
2025-08-08 18:26:42 +0300

    fixes

--- src/config/settings.py
+++ src/config/settings.py
@@ -9,8 +9,7 @@ class Settings(BaseSettings):
     SECRET_KEY_ACCESS: str = os.getenv("SECRET_KEY_ACCESS", str(os.urandom(32)))
     SECRET_KEY_REFRESH: str = os.getenv("SECRET_KEY_REFRESH", str(os.urandom(32)))
     JWT_SIGNING_ALGORITHM: str = os.getenv("JWT_SIGNING_ALGORITHM", "HS256")
-
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    LOGIN_TIME_DAYS: int = 7
 
     class Config:
         env_file = ".env"
